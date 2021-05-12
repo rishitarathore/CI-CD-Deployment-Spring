c18dcd6afa379ac9d3a65a681eb80cc5b604fc9f
rishitarathore <60125890+rishitarathore@users.noreply.github.com>
2021-05-12 23:05:33 +0530

    Update SpringbootAwsApplication.java

--- src/main/java/org/simplilearn/workshop/SpringbootAwsApplication.java
+++ src/main/java/org/simplilearn/workshop/SpringbootAwsApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootAwsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAwsApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SpringbootAwsApplication.class, args);
+// 	}
 
+	@Override
+  	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        	return builder.sources(Application.class);
+	}
 }
